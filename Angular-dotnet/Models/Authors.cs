45ac94706b7f097a23f4e6ca0356dc4551dfc1db
telmili <telmili@D-TN-2ZQP9L2>
2022-04-05 10:55:55 +0100

    Add project files.

---
+++ Angular-dotnet/Models/Authors.cs
@@ -0,0 +1,9 @@
+﻿namespace Angular_dotnet.Models
+{
+    public class Authors
+    {
+        public string? AuthorId { get; set; }
+        public string? Author { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Angular-dotnet/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Angular_dotnet.Models
+{
+    public class Category
+    {
+        public string? Name { get; set; }
+        public int Count { get; set; }
+    }
+}
